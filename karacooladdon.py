8ccb3e871528ac50785754f12e1bc9d1e2f457f4
mshendrikx <mauricio.servatius@gmail.com>
2024-02-19 16:08:25 -0300

    main

--- karacooladdon.py
+++ karacooladdon.py
@@ -1,3 +1,7 @@
+import requests
+
+SONG_FILE_NAME = ''
+
 def queue_reoder(self):
 
     index = 0
@@ -21,3 +25,18 @@ def queue_reoder(self):
       finalQueue.append(reorderLine[0])
 
     return finalQueue
+
+def lastfm_data(search_arg):
+  
+  url = "https://ws.audioscrobbler.com/2.0/?method=track.search&track=" + search_arg + '&api_key=ec023e5f4c7c36e55d3ebd3e933ca8d2&limit=10&format=json'
+  try:
+    response = requests.get(url)
+    data = response.json()  
+    tracks = []
+    for results in data['results']['trackmatches']['track']:
+      title = results['artist'] + ' - ' + results['name']
+      tracks.append(title)
+  except:
+    return None
+  
+  return tracks
